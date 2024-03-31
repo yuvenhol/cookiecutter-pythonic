9472345342a1975a3f301635ad1693f011ad57b5
yuvenhol <yuvenhol@gmail.com>
2024-03-31 22:29:18 +0800

    add root dicrect

--- {{ cookiecutter.project_slug }}/src/{{cookiecutter.project_slug}}/__init__.py
+++ {{ cookiecutter.project_slug }}/src/{{cookiecutter.project_slug}}/__init__.py
@@ -1,2 +1,8 @@
 """{{ cookiecutter.project_name }}"""
 __version__ = '{{ cookiecutter.version }}'
+
+
+import os
+
+src_path = os.path.dirname( os.path.abspath(__file__))
+root_absolute_path = os.path.abspath(os.path.join(src_path, os.pardir))
